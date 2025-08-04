3ac306796f48a450146be6906893973d6fb856b9
ZalioSW <ztoxiumisntoxic@gmail.com>
2025-08-04 20:47:57 +0200

    finalizing posting and trying to improve looks

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from flask import Flask, session, redirect, url_for, request, render_template
 from tinydb import TinyDB, Query
+import os
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 db = TinyDB('ig.json')
 users = db.table("users")
 posts = db.table("posts")
+upload_folder = "static/uploads"
+os.makedirs(upload_folder, exist_ok=True)
 
 app.secret_key = "zaliosw_najjaci123"
+app.config["upload_folder"] = upload_folder
 
 @app.route("/")
 def hello_world():
     if "username" not in session:
         return redirect("/signup")
-    return render_template("index.html", username=session["username"])
+    data = posts.all()
+    return render_template("index.html", username=session["username"], data=data)
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -25,19 +31,39 @@ def signup():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    error = None
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        print(username)
-        print(password)
         User = Query()
         user = users.search((User.username == username) & (User.password == password))
-        print(user)
         if user:
             session["username"] = username
             return redirect("/")
         else:
-            return "<p>Neki je narobe</p>"
-    return render_template("login.html")
+            error = "invalid credentials"
+    return render_template("login.html", error=error)
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    return redirect("/login")
+
+@app.route("/upload", methods=[ "GET", "POST"])
+def upload():
+    error = None
+    if request.method == "POST":
+        name = request.form["name"]
+        image = request.files["image"]
+        if image and name:
+            imgname = secure_filename(image.filename)
+            imgpath = os.path.join(app.config["upload_folder"], imgname)
+            image.save(imgpath)
+            posts.insert({"user": session["username"], "name": name, "path": imgpath})
+            return redirect("/")
+        else:
+            error = "Please select your file and input the name!"
+    return render_template("upload.html", error=error)
+
 
 app.run(debug=True)  
