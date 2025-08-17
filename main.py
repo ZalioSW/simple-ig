e73629083f4dfcbb020a6fa2f687a53b7828bce9
ZalioSW <ztoxiumisntoxic@gmail.com>
2025-08-17 18:40:32 +0200

    part 1 of the liking system

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def hello_world():
     if "username" not in session:
         return redirect("/signup")
     data = posts.all()
+    print(data)
     return render_template("index.html", username=session["username"], data=data)
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -52,6 +53,7 @@ def logout():
 @app.route("/upload", methods=[ "GET", "POST"])
 def upload():
     error = None
+    newId = len(posts) + 1
     if request.method == "POST":
         name = request.form["name"]
         image = request.files["image"]
@@ -59,7 +61,7 @@ def upload():
             imgname = secure_filename(image.filename)
             imgpath = os.path.join(app.config["upload_folder"], imgname).replace("\\", "/")
             image.save(imgpath)
-            posts.insert({"user": session["username"], "name": name, "path": imgpath})
+            posts.insert({"id": newId, "user": session["username"], "name": name, "path": imgpath})
             return redirect("/")
         else:
             error = "Please select your file and input the name!"
@@ -72,6 +74,26 @@ def viewProfile(username):
     Post = Query()
     user_posts = posts.search(Post.user == username)
     return render_template("viewProfile.html", username=username, posts=user_posts)
+
+@app.route("/likePost", methods=["POST"])
+def likePost():
+    username = request.form.get("username")
+    post_id = int(request.form.get("post_id"))
+    Post = Query()
+    thePost = posts.get(Post.id == post_id)
+
+    if thePost:
+        if "likes" not in thePost:
+            thePost["likes"] = []
+        if username in thePost["likes"]:
+            thePost["likes"].remove(username)
+        else:
+            thePost["likes"].append(username)
+    posts.update({"likes": thePost["likes"]}, Post.id == post_id)
+
+    return "OK", 200
+
+
     
 
 
